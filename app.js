2203bec6253dfcadd42c35d868cfa68d76e041e2
Vanditpujara <vanditpujara91@gmail.com>
2025-07-16 17:38:58 +0530

    push this

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-// app.use(express.static('public'));
-
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 
-const os = require('os');
-function getLocalIP() {
-  const interfaces = os.networkInterfaces();
-  for (let name in interfaces) {
-    for (let iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  return 'localhost';
-}
-const IP = getLocalIP();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -30,11 +15,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.listen(PORT, IP, () => {
-  console.log(`✅ Server running at http://${IP}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
